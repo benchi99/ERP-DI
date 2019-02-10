ddd0db6f9dc41ee5002422c5e4d9cfaf8d711fcc
Rubén (benchi99) Bermejo <mejoruben@gmail.com>
2019-02-10 12:48:28 +0100

    Cambios leves en los informes

--- ERP/Visor.Designer.cs
+++ ERP/Visor.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.InvoicesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.Facturas = new ERP.Informes();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
@@ -55,14 +55,15 @@
             this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            reportDataSource2.Name = "Facturas";
-            reportDataSource2.Value = this.InvoicesBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            reportDataSource1.Name = "Facturas";
+            reportDataSource1.Value = this.InvoicesBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "ERP.Facturas.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(18, 98);
+            this.reportViewer1.Location = new System.Drawing.Point(24, 121);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(768, 401);
+            this.reportViewer1.Size = new System.Drawing.Size(1023, 493);
             this.reportViewer1.TabIndex = 0;
             // 
             // InvoicesTableAdapter
@@ -73,11 +74,11 @@
             // 
             this.filterCBX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.filterCBX.FormattingEnabled = true;
-            this.filterCBX.ItemHeight = 23;
-            this.filterCBX.Location = new System.Drawing.Point(694, 64);
-            this.filterCBX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.filterCBX.ItemHeight = 24;
+            this.filterCBX.Location = new System.Drawing.Point(925, 79);
+            this.filterCBX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.filterCBX.Name = "filterCBX";
-            this.filterCBX.Size = new System.Drawing.Size(92, 29);
+            this.filterCBX.Size = new System.Drawing.Size(121, 30);
             this.filterCBX.TabIndex = 3;
             this.filterCBX.UseSelectable = true;
             this.filterCBX.SelectedIndexChanged += new System.EventHandler(this.filterCBX_SelectedIndexChanged);
@@ -85,10 +86,9 @@
             // filterLb
             // 
             this.filterLb.AutoSize = true;
-            this.filterLb.Location = new System.Drawing.Point(18, 69);
-            this.filterLb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.filterLb.Location = new System.Drawing.Point(24, 85);
             this.filterLb.Name = "filterLb";
-            this.filterLb.Size = new System.Drawing.Size(68, 19);
+            this.filterLb.Size = new System.Drawing.Size(68, 20);
             this.filterLb.TabIndex = 9999;
             this.filterLb.Text = "Filtrar por";
             // 
@@ -99,13 +99,14 @@
             // filtrCbx
             // 
             this.filtrCbx.FormattingEnabled = true;
-            this.filtrCbx.ItemHeight = 23;
+            this.filtrCbx.ItemHeight = 24;
             this.filtrCbx.Items.AddRange(new object[] {
             "ID",
             "Cliente"});
-            this.filtrCbx.Location = new System.Drawing.Point(91, 63);
+            this.filtrCbx.Location = new System.Drawing.Point(121, 78);
+            this.filtrCbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.filtrCbx.Name = "filtrCbx";
-            this.filtrCbx.Size = new System.Drawing.Size(121, 29);
+            this.filtrCbx.Size = new System.Drawing.Size(160, 30);
             this.filtrCbx.TabIndex = 1;
             this.filtrCbx.UseSelectable = true;
             this.filtrCbx.SelectedIndexChanged += new System.EventHandler(this.filtrCbx_SelectedIndexChanged);
@@ -114,9 +115,10 @@
             // 
             this.factIdLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.factIdLbl.AutoSize = true;
-            this.factIdLbl.Location = new System.Drawing.Point(573, 69);
+            this.factIdLbl.Location = new System.Drawing.Point(764, 85);
+            this.factIdLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.factIdLbl.Name = "factIdLbl";
-            this.factIdLbl.Size = new System.Drawing.Size(116, 19);
+            this.factIdLbl.Size = new System.Drawing.Size(124, 20);
             this.factIdLbl.TabIndex = 49999;
             this.factIdLbl.Text = "Seleccione Factura";
             // 
@@ -124,10 +126,11 @@
             // 
             this.cliFiltrCBX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cliFiltrCBX.FormattingEnabled = true;
-            this.cliFiltrCBX.ItemHeight = 23;
-            this.cliFiltrCBX.Location = new System.Drawing.Point(446, 64);
+            this.cliFiltrCBX.ItemHeight = 24;
+            this.cliFiltrCBX.Location = new System.Drawing.Point(595, 79);
+            this.cliFiltrCBX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cliFiltrCBX.Name = "cliFiltrCBX";
-            this.cliFiltrCBX.Size = new System.Drawing.Size(121, 29);
+            this.cliFiltrCBX.Size = new System.Drawing.Size(160, 30);
             this.cliFiltrCBX.TabIndex = 2;
             this.cliFiltrCBX.UseSelectable = true;
             this.cliFiltrCBX.SelectedIndexChanged += new System.EventHandler(this.cliFiltrCBX_SelectedIndexChanged);
@@ -136,17 +139,18 @@
             // 
             this.cliLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cliLbl.AutoSize = true;
-            this.cliLbl.Location = new System.Drawing.Point(329, 69);
+            this.cliLbl.Location = new System.Drawing.Point(439, 85);
+            this.cliLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.cliLbl.Name = "cliLbl";
-            this.cliLbl.Size = new System.Drawing.Size(111, 19);
+            this.cliLbl.Size = new System.Drawing.Size(121, 20);
             this.cliLbl.TabIndex = 69999;
             this.cliLbl.Text = "Seleccione cliente";
             // 
             // Visor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 518);
+            this.ClientSize = new System.Drawing.Size(1071, 638);
             this.Controls.Add(this.cliLbl);
             this.Controls.Add(this.cliFiltrCBX);
             this.Controls.Add(this.factIdLbl);
@@ -154,8 +158,9 @@
             this.Controls.Add(this.filterLb);
             this.Controls.Add(this.idCBX);
             this.Controls.Add(this.reportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Visor";
-            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
             this.Text = "Visor";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Visor_FormClosing);
             this.Load += new System.EventHandler(this.Visor_Load);
